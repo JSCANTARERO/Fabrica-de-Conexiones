7d20d832fb2efc1ce953ea7cf2fbffc0f20a0211
Windows 10 <Windows 10@DESKTOP-0AB2P58>
2019-05-27 12:14:38 -0600

    Agregar archivos de proyecto.

---
+++ 01 - Fabrica de Conecciones/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01___Fabrica_de_Conecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Una sencilla Fabrica de conecciones ******");
+
+            //Crear, establecer utilizar y luego cerrar la conexion
+
+            Console.ReadLine();
+        }
+    }
+}
