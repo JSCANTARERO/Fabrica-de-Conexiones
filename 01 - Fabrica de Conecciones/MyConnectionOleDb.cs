7576fb12612a1113cfa8fd9fa48845456e848990
Windows 10 <Windows 10@DESKTOP-0AB2P58>
2019-06-10 09:07:12 -0600

    Finalizacion de las clases MyConnection

--- 01 - Fabrica de Conecciones/MyConnectionOleDb.cs
+++ 01 - Fabrica de Conecciones/MyConnectionOleDb.cs
@@ -1,47 +1,48 @@
 ﻿using System;
-//Namespaces necesarios para realizar la conexion 
+// Namespaces necesarios para realizar la conexión OleDB
 using System.Data;
 using System.Data.OleDb;
+
 namespace _01___Fábrica_de_Conexiones
 {
-    class MyConnectionOleDb
+    class MyConnectionOleDb : MyConnection
     {
-        //Constructor
+        // Constructor
         public MyConnectionOleDb(DataProvider theDataProvider) : base(theDataProvider)
         {
-            //Crear la cadena de conexion
-            OleDbConnection connectionString = new OleDbConnection(@"provider = sqloledb
-                data source = (local)/sqlexpress; integrated security = sspi;");//sspi para modo de autenticacion de windows
+            // Crear la cadena de conexión
+            OleDbConnection connectionString = new OleDbConnection(@"provider = sqloledb;
+                data source = (local)\sqlexpress; integrated security = sspi;");
+
             try
             {
-                //Esctablecer conexión
+                // Establecer la conexión
                 connectionString.Open();
-                Console.WriteLine("Conexion establecida");
+                Console.WriteLine("Conexión establecida");
 
-                //Detalles de la conexion
+                // Detalles de la conexión
                 this.DetallesConexion(connectionString);
             }
             catch (OleDbException e)
             {
-                //Desplegar el error en la conexion
+                // Desplegar el error en la conexión
                 Console.WriteLine("Error: {0} {1}", e.Message, e.StackTrace);
             }
             finally
             {
-                //Cerrar la conexion
+                // Cerrar la conexión
                 connectionString.Close();
             }
         }
 
         protected void DetallesConexion(OleDbConnection connectionString)
         {
-            Console.WriteLine("Conexion establecida");
+            Console.WriteLine("Conexión establecida");
             Console.WriteLine("\tConnection string: {0}", connectionString.ConnectionString);
             Console.WriteLine("\tBase de datos: {0}", connectionString.Database);
             Console.WriteLine("\tFuente de datos: {0}", connectionString.DataSource);
-            Console.WriteLine("\tVersion del Servidor: {0}", connectionString.ServerVersion);
-            Console.WriteLine("\tEstado: {0}", connectionString.State);// Con oldb no se puede saaber el workstation
-            //Finalizacion de la clase MyConnectionOleDb con los detalles de conexion
+            Console.WriteLine("\tVersión del servidor: {0}", connectionString.ServerVersion);
+            Console.WriteLine("\tEstado: {0}", connectionString.State);
         }
     }
 }
